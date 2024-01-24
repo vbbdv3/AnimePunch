e2b9fcd1553951a165abf8fa0678eb0345250e7e
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-01-24 21:11:32 +0500

    Update TrashHub.lua

--- TrashHub.lua
+++ TrashHub.lua
@@ -49,6 +49,10 @@ local mod = main:Button("EvolvedMeliedes", function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/vbbdv3/AnimePunch/main/EvolvedMeliedes'))()
 end)
 
+local mod = main:Button("Dungeon", function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vbbdv3/AnimePunch/main/Dungeon'))()
+end)
+
 local mod = main:Button("RaidBoss", function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/vbbdv3/AnimePunch/main/RaidBoss'))()
 end)
