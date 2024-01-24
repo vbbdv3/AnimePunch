18bb7882f02bbf93af7c2a5f85f9107b93197170
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-01-24 16:13:03 +0500

    Update TrashHub.lua

--- TrashHub.lua
+++ TrashHub.lua
@@ -3,7 +3,7 @@ while not game:IsLoaded() do
 end
 
 local libary = loadstring(game:HttpGet("https://pastebin.com/raw/RvJ0qewm", true))()
-local main = libary:CreateWindow("Anime Punch V0.9")
+local main = libary:CreateWindow("Anime Punch V1.0")
 
 local mod = main:Button("AntiAfk", function()
 loadstring(game:HttpGet("https://scriptblox.com/raw/Universal-Script-Anti-Afk-Remastered-Script-or-Diffrent-Cool-UI-2097г"))()
